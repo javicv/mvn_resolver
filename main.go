557efaa3f0298a3782424223b9ce56534ea39d95
Javier Collado <javicv@gmail.com>
2019-05-23 23:43:27 +0200

    License added. Some variable names changed

--- main.go
+++ main.go
@@ -1,60 +1,77 @@
+/**
+*  Copyright 2019 Francisco Javier Collado Valle
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+**/
+
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
-    "io/ioutil"
-    "net/http"
-    "encoding/xml"
-    "github.com/javicv/mvn_resolver/xml"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	maven "github.com/javicv/mvn_resolver/xml"
 )
 
 func main() {
 
-    var REPO, is_set = os.LookupEnv("MAVEN_REPO_URL")
-    if !is_set {
-        fmt.Fprintf(os.Stderr, "Error: Environment variable MAVEN_REPO_URL must be set\n")
-        os.Exit(1)
-    }
+	var REPO, isSet = os.LookupEnv("MAVEN_REPO_URL")
+	if !isSet {
+		fmt.Fprintf(os.Stderr, "Error: Environment variable MAVEN_REPO_URL must be set\n")
+		os.Exit(1)
+	}
 
-    if len(os.Args)<5 {
-        fmt.Fprintf(os.Stderr, "Error: Not enough parameters provided\n")
-        fmt.Fprintf(os.Stderr, "Uso: %s GROUP_ID ARTIFACT_ID VERSION PACKAGING\n", os.Args[0])
-        os.Exit(2)
-    }
-    var group_id = strings.Split(os.Args[1], ".")
-    var artifact_id = os.Args[2]
-    var version = os.Args[3]
-    var packaging = os.Args[4]
+	if len(os.Args) < 5 {
+		fmt.Fprintf(os.Stderr, "Error: Not enough parameters provided\n")
+		fmt.Fprintf(os.Stderr, "Uso: %s GROUP_ID ARTIFACT_ID VERSION PACKAGING\n", os.Args[0])
+		os.Exit(2)
+	}
+	var groupID = strings.Split(os.Args[1], ".")
+	var artifactID = os.Args[2]
+	var version = os.Args[3]
+	var packaging = os.Args[4]
 
-    var metadata_url = fmt.Sprintf("%s/%s/%s/%s/maven-metadata.xml", REPO, strings.Join(group_id,"/"),artifact_id,version)
-    resp, err := http.Get(metadata_url)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-        os.Exit(3)
-    }
-    if resp.StatusCode != 200 {
-        fmt.Fprintf(os.Stderr, "%s: %s\n", resp.Status, metadata_url)
-        os.Exit(3)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error processing response\n")
-        os.Exit(4)
-    }
-    var metadata maven.Metadata
-    err = xml.Unmarshal(body, &metadata)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error: %s\n", err)
-        os.Exit(5)
-    }
+	var metadataURL = fmt.Sprintf("%s/%s/%s/%s/maven-metadata.xml", REPO, strings.Join(groupID, "/"), artifactID, version)
+	resp, err := http.Get(metadataURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(3)
+	}
+	if resp.StatusCode != 200 {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", resp.Status, metadataURL)
+		os.Exit(3)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error processing response\n")
+		os.Exit(4)
+	}
+	var metadata maven.Metadata
+	err = xml.Unmarshal(body, &metadata)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(5)
+	}
 
-    for _,snapshotVersion := range metadata.Versioning.SnapshotVersions.SnapshotVersion {
-        if snapshotVersion.Extension == packaging {
-            var artifact_url = fmt.Sprintf("%s/%s/%s/%s/%s-%s.%s", REPO, strings.Join(group_id,"/"), artifact_id, version, artifact_id, snapshotVersion.Value, snapshotVersion.Extension)
-            fmt.Print(artifact_url)
-        }
-    }
+	for _, snapshotVersion := range metadata.Versioning.SnapshotVersions.SnapshotVersion {
+		if snapshotVersion.Extension == packaging {
+			var artifactURL = fmt.Sprintf("%s/%s/%s/%s/%s-%s.%s", REPO, strings.Join(groupID, "/"), artifactID, version, artifactID, snapshotVersion.Value, snapshotVersion.Extension)
+			fmt.Print(artifactURL)
+		}
+	}
 }
